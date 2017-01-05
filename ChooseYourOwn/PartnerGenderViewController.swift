e75453469180e2aaded1fc13376696474bf04f2e
student5 <student3@jh-mba158-02.d214.org>
2017-01-05 10:39:57 -0600

    created partner gender view controller. named view controllers to connect files and controllers.

---
+++ ChooseYourOwn/PartnerGenderViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PartnerGenderViewController.swift
+//  ChooseYourOwn
+//
+//  Created by student3 on 1/5/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class PartnerGenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
