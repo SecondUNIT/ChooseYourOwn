0a6e9ed16a1dc6e3e9d917faa3d504ec18bd59ef
student5 <student3@jh-mba158-02.d214.org>
2017-01-05 10:43:03 -0600

    named remaining segues, connected code with partner gender view controller, created new view controller

---
+++ ChooseYourOwn/RandomizerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RandomizerViewController.swift
+//  ChooseYourOwn
+//
+//  Created by student3 on 1/5/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class RandomizerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
